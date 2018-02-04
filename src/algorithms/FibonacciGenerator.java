0a446bc7c8f8d4cd020a92e20affba75c5d7fdae
katherine55 <league@WTS10.attlocal.net>
2018-02-04 11:30:21 -0800

    new

---
+++ src/algorithms/FibonacciGenerator.java
@@ -0,0 +1,18 @@
+package algorithms;
+
+public class FibonacciGenerator {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				for (int k = 0; k < 4; k++) {
+				int z = 100*i + 10*j + k;
+				System.out.println(z);
+				}
+
+			}
+		}
+
+	}
+
+}
